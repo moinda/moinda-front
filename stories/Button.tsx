b6b21439508cfc2339724f7483105bd6be9a1602
JunKyus <kuk941025@gmail.com>
2020-11-16 11:55:13 +0900

    Update eslint extends

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -27,13 +27,7 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button: React.FC<ButtonProps> = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}) => {
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
--- stories/Header.tsx
+++ stories/Header.tsx
@@ -4,13 +4,13 @@ import { Button } from './Button';
 import './header.css';
 
 export interface HeaderProps {
-  user?: {};
+  user?: unknown;
   onLogin: () => void;
   onLogout: () => void;
   onCreateAccount: () => void;
 }
 
-export const Header: React.FC<HeaderProps> = ({ user, onLogin, onLogout, onCreateAccount }) => (
+export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
   <header>
     <div className="wrapper">
       <div>
